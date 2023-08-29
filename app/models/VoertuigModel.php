c0427d25e9732bd3774479abb6cc01edfdf8ff0e
ThePope05 <swobben05@gmail.com>
2023-08-29 10:49:00 +0200

    Added Voertuig model

---
+++ app/models/VoertuigModel.php
@@ -0,0 +1,82 @@
+<?php
+
+class InstructeurModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getVoertuigen()
+    {
+        $sql = "SELECT VOER.Id
+                      ,VOER.Type
+                      ,VOER.Brandstof
+                      ,TYVO.TypeVoertuig
+                FROM  voertuig AS VOER
+                JOIN TypeVoertuig as TYVO
+                ON VOER.TypeVoertuigId = TYVO.Id;
+                ";
+
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    // public function getToegewezenVoertuigen($Id)
+    // {
+    //     $sql = "SELECT TYVO.TypeVoertuig,
+    //                    VOER.Type,
+    //                    VOER.Kenteken,
+    //                    VOER.Bouwjaar,
+    //                    VOER.Brandstof,
+    //                    TYVO.RijbewijsCategorie
+    //             FROM   Voertuig AS VOER
+    //             INNER JOIN TypeVoertuig AS TYVO
+    //             ON VOER.TypeVoertuigId = TYVO.Id
+    //             WHERE VOER.Id IN (
+    //             SELECT VoertuigId FROM voertuiginstructeur 
+    //             WHERE InstructeurId = $Id)
+    //             ORDER BY TYVO.RijbewijsCategorie";
+
+    //     $this->db->query($sql);
+
+    //     return $this->db->resultSet();
+    // }
+
+    // public function getVrijeVoertuigen($Id)
+    // {
+    //     $sql = "SELECT TYVO.TypeVoertuig,
+    //                    VOER.Type,
+    //                    VOER.Kenteken,
+    //                    VOER.Bouwjaar,
+    //                    VOER.Brandstof,
+    //                    TYVO.RijbewijsCategorie,
+    //                    VOER.Id
+    //             FROM   Voertuig AS VOER
+    //             INNER JOIN TypeVoertuig AS TYVO
+    //             ON VOER.TypeVoertuigId = TYVO.Id
+    //             WHERE VOER.Id NOT IN (
+    //                 SELECT VoertuigId from voertuiginstructeur
+    //             );";
+
+    //     $this->db->query($sql);
+
+    //     return $this->db->resultSet();
+    // }
+
+    // public function addCarToInstructeur($CarId, $PersonId)
+    // {
+    //     $sql = "INSERT INTO voertuiginstructeur
+    //             VALUES(null, 
+    //             (SELECT Id FROM Voertuig WHERE Id = $CarId),
+    //             (SELECT Id FROM instructeur WHERE Id = $PersonId), 
+    //             (SELECT Bouwjaar FROM Voertuig WHERE Id = $CarId),
+    //             1, null, SYSDATE(6), SYSDATE(6));";
+
+    //     $this->db->query($sql);
+
+    //     $this->db->excecuteWithoutReturn();
+    // }
+}
