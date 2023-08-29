0034526e1e758907ecb6ed8fa59e7e7c29ed5fc3
ThePope05 <swobben05@gmail.com>
2023-08-29 10:37:46 +0200

    Changed styling
    Colors, button, links

--- app/config/config.php
+++ app/config/config.php
@@ -4,4 +4,4 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '15122005');
 define('DB_NAME', 'Mvc-2209a-P4');
-define('URLROOT', 'http://www.mvc-2209a-p4.com');
+define('URLROOT', 'http://www.mvc.com');
